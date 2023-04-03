7f0108df1927bd99693f959de2106451b5282bf8
arda-pecel <129625706+arda-pecel@users.noreply.github.com>
2023-04-03 14:13:59 +0300

    Update code.py

--- code.py
+++ code.py
@@ -5,12 +5,62 @@ h_distanceback; #horizontal distance with rear car. This value comes from Augela
 v_distanceback; #vertical distance with rear car. This value comes from Augelab Studio
 split_width; #line width. This value comes from Augelab Studio
 
-#determination of velocity
+#determination of velocity ***THIS PART OF THE CODE DOES NOT WORK PROPERLY. IN NEXT UPDATE SECOND CODE WILL BE TRIED***
 
+values = [[h_distance],[v_distance],[h_distanceback],[v_distanceback]]
+last_values = [0,0]
+last_velocity = 0
+delta = 0.1  # Will need to play with this value.
+terminal_velocity = None
+for pos, time in values:
+    velocity = (pos - last_values[0]) / (time - last_values[1])
+    if abs(velocity - last_velocity) < delta:
+        terminal_velocity = velocity
+        break
+    last_values = [pos, time]
+    last_velocity = velocity
 
+#second code will be tried
 
+import numpy as np
 
+# define function to calculate distance between two points
+def distance(p1, p2):
+    return np.sqrt((p1[0]-p2[0])**2 + (p1[1]-p2[1])**2)
 
+# set initial values
+last_pos = None
+last_time = None
+other_car_positions = [(x1, y1), (x2, y2), ...] # list of positions of other cars. This values will be getted from Augelab Studio.
+other_car_speeds = [v1, v2, ...] # list of speeds of other cars. This values will be getted from Augelab Studio.
+
+# get current position and time
+current_pos = (my_pos_x, my_pos_y) # your car's current position
+current_time = time.time() # current time in seconds
+
+# calculate distances between your car and the other cars
+distances = []
+for pos in other_car_positions:
+    distances.append(distance(current_pos, pos))
+
+# calculate relative velocities between your car and the other cars
+if last_pos is not None and last_time is not None:
+    dt = current_time - last_time
+    rel_velocities = []
+    for i in range(len(other_car_speeds)):
+        rel_velocities.append((distances[i] - distance(last_pos[i], other_car_positions[i])) / dt)
+else:
+    rel_velocities = [0] * len(other_car_speeds)
+
+# calculate your velocity
+your_speed = my_speed # your car's speed
+your_velocity = your_speed - sum(rel_velocities)
+
+# update last_pos and last_time
+last_pos = other_car_positions
+last_time = current_time
+    
+    
 #motor control
 
 import RPi.GPIO as GPIO          
@@ -164,6 +214,64 @@ elif  abs(h_distance) > 1000 && v_distance < 1000:
 else:
     continue
     
+
+# improve, decrease speed on normal driving
+
+if velocity>100:
+    if abs(h_distance) && v_distance > 1000:
+    p.ChangeDutyCycle(50)
+        GPIO.output(in1,GPIO.LOW)
+        GPIO.output(in2,GPIO.HIGH)
+        temp1=1
+        x='h'
+    elif abs(h_distance) && v_distance > 500:
+        continue
+    else:
+        p.ChangeDutyCycle(50)
+        GPIO.output(in1,GPIO.LOW)
+        GPIO.output(in2,GPIO.LOW)
+        temp1=1
+        x='m'
+        
+elif velocity>50:
+    if abs(h_distance) && v_distance > 500:
+    p.ChangeDutyCycle(50)
+        GPIO.output(in1,GPIO.LOW)
+        GPIO.output(in2,GPIO.HIGH)
+        temp1=1
+        x='m'
+    elif abs(h_distance) && v_distance > 250:
+        continue
+    else:
+        p.ChangeDutyCycle(50)
+        GPIO.output(in1,GPIO.LOW)
+        GPIO.output(in2,GPIO.LOW)
+        temp1=1
+        x='l'
+else:
+    if abs(h_distance) && v_distance > 125:
+    p.ChangeDutyCycle(50)
+        GPIO.output(in1,GPIO.LOW)
+        GPIO.output(in2,GPIO.HIGH)
+        temp1=1
+        continue
+    elif abs(h_distance) && v_distance > 50:
+        p.ChangeDutyCycle(50)
+        GPIO.output(in1,GPIO.LOW)
+        GPIO.output(in2,GPIO.LOW)
+        temp1=1
+        continue
+    else:
+        p.ChangeDutyCycle(50)
+        GPIO.output(in1,GPIO.LOW)
+        GPIO.output(in2,GPIO.LOW)
+        temp1=1
+        x='s'
+        break
+
+    
+    
+
 
 
 
